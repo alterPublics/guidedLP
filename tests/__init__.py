61b548ef9839b6d310a4b0b07c77833c9fe5cd42
alterPublics <jakobbk@ruc.dk>
2025-10-03 14:49:08 +0200

    initial setup

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+Test suite for the Guided Label Propagation library.
+
+This package contains unit tests, integration tests, and performance benchmarks
+for all modules in the GLP library.
+"""
